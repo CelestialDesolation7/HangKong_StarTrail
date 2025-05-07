3658cec3e4db8ae6fb69c60c4bfef4a452e578af
CelestialDesolation7 <137397992+CelestialDesolation7@users.noreply.github.com>
2025-05-07 19:16:40 +0800

    删除冗余文件

--- HangKong_StarTrail/Views/KnowledgeBaseForm.xaml.cs
+++ HangKong_StarTrail/Views/KnowledgeBaseForm.xaml.cs
@@ -1,136 +1,90 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Input;
 using HangKong_StarTrail.Models;
 using HangKong_StarTrail.Services;
 
-namespace HangKong_StarTrail.Views
+namespace HangKong_StarTrail
 {
     public partial class KnowledgeBaseForm : Window
     {
         private readonly IKnowledgeService _knowledgeService;
-        private List<KnowledgeItem> _allItems;
-        private List<KnowledgeItem> _favorites;
 
         public KnowledgeBaseForm()
         {
             InitializeComponent();
-            _knowledgeService = new KnowledgeService();
-            LoadKnowledgeData();
-            InitializeTreeView();
+            _knowledgeService = new KnowledgeService(); // 这里需要实现具体的服务类
+            LoadAllKnowledge();
         }
 
-        private void LoadKnowledgeData()
+        #region 窗口控制
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            _allItems = _knowledgeService.GetAllKnowledgeItems();
-            _favorites = _knowledgeService.GetFavorites();
-        }
-
-        private void InitializeTreeView()
-        {
-            var rootNodes = new List<TreeViewItem>
+            if (e.ChangedButton == MouseButton.Left)
             {
-                CreateCategoryNode("恒星", "star"),
-                CreateCategoryNode("行星", "planet"),
-                CreateCategoryNode("星系", "galaxy"),
-                CreateCategoryNode("黑洞", "blackhole"),
-                CreateCategoryNode("其他", "other")
-            };
-
-            KnowledgeTree.ItemsSource = rootNodes;
-        }
-
-        private TreeViewItem CreateCategoryNode(string title, string category)
-        {
-            var node = new TreeViewItem { Header = title };
-            var items = _allItems.Where(item => item.Category == category).ToList();
-            
-            foreach (var item in items)
-            {
-                node.Items.Add(new TreeViewItem { Header = item.Title, Tag = item });
-            }
-            
-            return node;
-        }
-
-        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var searchText = SearchBox.Text.ToLower();
-            if (string.IsNullOrWhiteSpace(searchText))
-            {
-                InitializeTreeView();
-                return;
-            }
-
-            var filteredItems = _allItems
-                .Where(item => item.Title.ToLower().Contains(searchText) || 
-                              item.Content.ToLower().Contains(searchText))
-                .ToList();
-
-            var searchNode = new TreeViewItem { Header = "搜索结果" };
-            foreach (var item in filteredItems)
-            {
-                searchNode.Items.Add(new TreeViewItem { Header = item.Title, Tag = item });
-            }
-
-            KnowledgeTree.ItemsSource = new[] { searchNode };
-        }
-
-        private void KnowledgeTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            if (e.NewValue is TreeViewItem selectedItem && selectedItem.Tag is KnowledgeItem item)
-            {
-                ContentTitle.Text = item.Title;
-                ContentBrowser.NavigateToString(FormatContent(item));
+                this.DragMove();
             }
         }
 
-        private string FormatContent(KnowledgeItem item)
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
-            return $@"
-                <html>
-                <head>
-                    <style>
-                        body {{ 
-                            font-family: Arial, sans-serif; 
-                            line-height: 1.6; 
-                            color: #333; 
-                            padding: 20px;
-                        }}
-                        h1 {{ color: #4169E1; }}
-                        img {{ max-width: 100%; height: auto; }}
-                    </style>
-                </head>
-                <body>
-                    <h1>{item.Title}</h1>
-                    <p>{item.Content}</p>
-                    {(!string.IsNullOrEmpty(item.ImagePath) ? $"<img src='{item.ImagePath}' alt='{item.Title}'/>" : "")}
-                </body>
-                </html>";
+            this.WindowState = WindowState.Minimized;
         }
 
-        private void QuizButton_Click(object sender, RoutedEventArgs e)
-        {
-            var quizForm = new QuizForm();
-            quizForm.ShowDialog();
-        }
-
-        private void FavoritesButton_Click(object sender, RoutedEventArgs e)
-        {
-            var favoritesNode = new TreeViewItem { Header = "收藏夹" };
-            foreach (var item in _favorites)
-            {
-                favoritesNode.Items.Add(new TreeViewItem { Header = item.Title, Tag = item });
-            }
-
-            KnowledgeTree.ItemsSource = new[] { favoritesNode };
-        }
-
-        private void BackButton_Click(object sender, RoutedEventArgs e)
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
+        #endregion
+
+        #region 知识库功能
+
+        private void LoadAllKnowledge()
+        {
+            var items = _knowledgeService.GetAllKnowledgeItems();
+            KnowledgeList.ItemsSource = items;
+        }
+
+        private void ShowAllKnowledge_Click(object sender, RoutedEventArgs e)
+        {
+            LoadAllKnowledge();
+        }
+
+        private void ShowStarKnowledge_Click(object sender, RoutedEventArgs e)
+        {
+            var items = _knowledgeService.GetItemsByCategory("恒星");
+            KnowledgeList.ItemsSource = items;
+        }
+
+        private void ShowGalaxyKnowledge_Click(object sender, RoutedEventArgs e)
+        {
+            var items = _knowledgeService.GetItemsByCategory("星系");
+            KnowledgeList.ItemsSource = items;
+        }
+
+        private void ShowPhenomena_Click(object sender, RoutedEventArgs e)
+        {
+            var items = _knowledgeService.GetItemsByCategory("宇宙现象");
+            KnowledgeList.ItemsSource = items;
+        }
+
+        private void ShowFavorites_Click(object sender, RoutedEventArgs e)
+        {
+            var items = _knowledgeService.GetFavorites();
+            KnowledgeList.ItemsSource = items;
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var keyword = SearchBox.Text;
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                var items = _knowledgeService.SearchItems(keyword);
+                KnowledgeList.ItemsSource = items;
+            }
+        }
+
+        #endregion
     }
 } 
